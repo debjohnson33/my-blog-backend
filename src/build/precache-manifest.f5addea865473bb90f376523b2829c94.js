785d539740aec13704ba4ca056c5c063144b4187
debjohnson33 <debjohnson_11@yahoo.com>
2019-09-16 17:22:18 -0500

    Adds front end/changes to server

---
+++ src/build/precache-manifest.f5addea865473bb90f376523b2829c94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc970c658f1a7761ea117a927832d5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72a68ec04bdc16d135d3",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "d7a23a3bae00826e4727",
+    "url": "/static/js/2.16e464fe.chunk.js"
+  },
+  {
+    "revision": "72a68ec04bdc16d135d3",
+    "url": "/static/js/main.fa522726.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
